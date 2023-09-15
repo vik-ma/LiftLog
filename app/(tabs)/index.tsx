e84deded78880460154d80af62d377a659b6afde
Viktor <vikt.mag@gmail.com>
2023-09-15 12:07:46 +0200

    Made buttons to create and delete profiles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,17 +3,20 @@ import Button from "../../components/Button";
 import { Text, View } from "../../components/Themed";
 
 export default function TabOneScreen() {
-  const [buttonText, setButtonText] = useState<string>("");
-
   return (
     <View className="flex justify-center items-center h-full">
       <View className="flex justify-center items-center">
-        <Text>{buttonText}</Text>
+        <Text className="text-2xl font-bold pb-3">Profiles</Text>
         <Button
-          label="Test Button"
+          label="Create Profile"
+          styling="bg-green-500 mb-4"
+          onPress={() => {}}
+        />
+        <Button
+          label="Delete Profile"
           styling="bg-red-500"
-          onPress={() => setButtonText("Test")}
-        ></Button>
+          onPress={() => {}}
+        />
       </View>
     </View>
   );
