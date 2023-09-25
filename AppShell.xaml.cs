9f77aa29354db0d0b536133405c0f5955347b1bd
Viktor <vikt.mag@gmail.com>
2023-09-25 22:06:26 +0200

    Deleted old code

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,17 +1,10 @@
-﻿using LocalLiftLog.Views;
-
-namespace LocalLiftLog
+﻿namespace LocalLiftLog
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-
-            Routing.RegisterRoute(nameof(RoutineListPage), typeof(RoutineListPage));
-            Routing.RegisterRoute(nameof(EditRoutineListPage), typeof(EditRoutineListPage));
-            Routing.RegisterRoute(nameof(AddRoutinePage), typeof(AddRoutinePage));
-            Routing.RegisterRoute(nameof(EditRoutinePage), typeof(EditRoutinePage));
         }
     }
 }
