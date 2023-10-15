0c373f4ac5bdb0893932ade6717aadce83a9bc85
Viktor <vikt.mag@gmail.com>
2023-10-15 15:07:36 +0200

    Added WeeklySchedule class

---
+++ Models/WeeklySchedule.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class WeeklySchedule
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int Day1TemplateId { get; set; }
+        public int Day2TemplateId { get; set; }
+        public int Day3TemplateId { get; set; }
+        public int Day4TemplateId { get; set; }  
+        public int Day5TemplateId { get; set; }
+        public int Day6TemplateId { get; set; }
+        public int Day7TemplateId { get; set; }
+    }
+}
