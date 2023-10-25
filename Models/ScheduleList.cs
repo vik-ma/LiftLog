f24c86c6c3553bd36fc8d1bb24728447e2d91b51
Viktor <vikt.mag@gmail.com>
2023-10-25 19:58:59 +0200

    Added ScheduleList class

---
+++ Models/ScheduleList.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class ScheduleList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public bool IsScheduleWeekly { get; set; }
+        public int ScheduleId { get; set; }
+    }
+}
