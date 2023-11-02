4553e8a52e7c43dac9538f3963ed0ed5408dd389
Viktor <vikt.mag@gmail.com>
2023-11-03 00:50:01 +0100

    Added MultiBindingConverter class

---
+++ Converters/MultiBindingConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Converters
+{
+    public class MultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Tuple<object, object>(values[0], values[1]);
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
