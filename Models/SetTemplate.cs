7ed17ef8c072902d65dc6415b6fe8a9f5a8c7e93
Viktor <vikt.mag@gmail.com>
2023-11-16 16:29:00 +0100

    Added SetTemplate and SetTemplateCollection models

---
+++ Models/SetTemplate.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class SetTemplate
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public string Note { get; set; }
+        public bool IsTrackingWeight { get; set; }
+        public bool IsTrackingReps { get; set; }
+        public bool IsTrackingRir {  get; set; }
+        public bool IsTrackingRpe { get; set; }
+        public bool IsTrackingTime { get; set; }
+        public bool IsTrackingDistance { get; set; }
+        public bool IsTrackingCardioResistance { get; set; }
+        public bool IsUsingBodyWeightAsWeight { get; set; }
+    }
+}
---
+++ Models/SetTemplateCollection.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class SetTemplateCollection
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int SetTemplateId { get; set; }
+        public int SetListIndex { get; set; }
+    }
+}
