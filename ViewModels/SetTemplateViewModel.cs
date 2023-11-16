9e85998ff5e90ebabba66d55da6fcf2e83189142
Viktor <vikt.mag@gmail.com>
2023-11-17 00:32:20 +0100

    Added LoadSetTemplatesAsync and LoadSetTemplateCollectionsAsync function to SetTemplateViewModel

--- ViewModels/SetTemplateViewModel.cs
+++ ViewModels/SetTemplateViewModel.cs
@@ -22,6 +22,70 @@ namespace LocalLiftLog.ViewModels
             _context = context;
         }
 
+        [ObservableProperty]
+        private ObservableCollection<SetTemplate> setTemplateList = new();
+
+        [ObservableProperty]
+        private ObservableCollection<SetTemplateCollection> setTemplateCollectionList = new();
+
+        public async Task LoadSetTemplatesAsync()
+        {
+            await ExecuteAsync(async () =>
+            {
+                SetTemplateList.Clear();
+
+                var setTemplates = await _context.GetAllAsync<SetTemplate>();
+
+                if (setTemplates is not null && setTemplates.Any())
+                {
+                    setTemplates ??= new ObservableCollection<SetTemplate>();
+
+                    foreach (var set in setTemplates)
+                    {
+                        SetTemplateList.Add(set);
+                    }
+                }
+            });
+        }
+
+        public async Task LoadSetTemplateCollectionsAsync()
+        {
+            await ExecuteAsync(async () =>
+            {
+                SetTemplateCollectionList.Clear();
+
+                var setTemplatesCollections = await _context.GetAllAsync<SetTemplateCollection>();
+
+                if (setTemplatesCollections is not null && setTemplatesCollections.Any())
+                {
+                    setTemplatesCollections ??= new ObservableCollection<SetTemplateCollection>();
+
+                    foreach (var setCollection in setTemplatesCollections)
+                    {
+                        SetTemplateCollectionList.Add(setCollection);
+                    }
+                }
+            });
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
         [RelayCommand]
         static async Task GoBack()
         {
