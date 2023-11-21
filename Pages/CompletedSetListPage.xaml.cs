d248960d093526415e748e69dfafd457a71f29d4
Viktor <vikt.mag@gmail.com>
2023-11-21 22:25:39 +0100

    Added basic functionality to CompletedSetListPage and CompletedSetViewModel

--- Pages/CompletedSetListPage.xaml.cs
+++ Pages/CompletedSetListPage.xaml.cs
@@ -1,9 +1,20 @@
 namespace LocalLiftLog.Pages;
+using LocalLiftLog.ViewModels;
 
 public partial class CompletedSetListPage : ContentPage
 {
-	public CompletedSetListPage()
+	private readonly CompletedSetViewModel _viewModel;
+	public CompletedSetListPage(CompletedSetViewModel viewModel)
 	{
 		InitializeComponent();
-	}
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        //await _viewModel.LoadCompletedSetsAsync();
+        //await _viewModel.LoadCompletedSetCollectionsAsync();
+    }
 }
--- ViewModels/CompletedSetViewModel.cs
+++ ViewModels/CompletedSetViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Pages;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq.Expressions;
 
 namespace LocalLiftLog.ViewModels
 {
-    internal class CompletedSetViewModel
+    public partial class CompletedSetViewModel : ObservableObject
     {
+        private readonly DatabaseContext _context;
+        public CompletedSetViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
     }
 }
