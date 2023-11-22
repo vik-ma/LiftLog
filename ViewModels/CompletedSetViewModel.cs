156225f149068880ab33c7ee677cf1a0f2c80eb9
Viktor <vikt.mag@gmail.com>
2023-11-22 01:50:24 +0100

    Removed CompletedSetCollection related functin from CompletedSetViewModel

--- ViewModels/CompletedSetViewModel.cs
+++ ViewModels/CompletedSetViewModel.cs
@@ -25,9 +25,6 @@ namespace LocalLiftLog.ViewModels
         [ObservableProperty]
         private ObservableCollection<CompletedSet> completedSetList = new();
 
-        [ObservableProperty]
-        private ObservableCollection<CompletedSetCollection> completedSetCollectionList = new();
-
         public async Task LoadCompletedSetsAsync()
         {
             await ExecuteAsync(async () =>
@@ -43,26 +40,6 @@ namespace LocalLiftLog.ViewModels
                     foreach (var set in completedSets)
                     {
                         CompletedSetList.Add(set);
-                    }
-                }
-            });
-        }
-
-        public async Task LoadCompletedSetCollectionsAsync()
-        {
-            await ExecuteAsync(async () =>
-            {
-                CompletedSetCollectionList.Clear();
-
-                var completedSetsCollections = await _context.GetAllAsync<CompletedSetCollection>();
-
-                if (completedSetsCollections is not null && completedSetsCollections.Any())
-                {
-                    completedSetsCollections ??= new ObservableCollection<CompletedSetCollection>();
-
-                    foreach (var setCollection in completedSetsCollections)
-                    {
-                        CompletedSetCollectionList.Add(setCollection);
                     }
                 }
             });
@@ -98,17 +75,6 @@ namespace LocalLiftLog.ViewModels
         }
 
         [RelayCommand]
-        private async Task CreateCompletedSetCollectionAsync()
-        {
-            await ExecuteAsync(async () =>
-            {
-                CompletedSetCollection setCollection = new();
-                await _context.AddItemAsync<CompletedSetCollection>(setCollection);
-                CompletedSetCollectionList.Add(setCollection);
-            });
-        }
-
-        [RelayCommand]
         private async Task UpdateCompletedSetAsync(int id)
         {
             CompletedSet completedSet = CompletedSetList.FirstOrDefault(p => p.Id == id);
@@ -125,46 +91,6 @@ namespace LocalLiftLog.ViewModels
 
                 await LoadCompletedSetsAsync();
             });
-        }
-
-        [RelayCommand]
-        private async Task UpdateCompletedSetCollectionAsync(int id)
-        {
-            CompletedSetCollection completedSetCollection = CompletedSetCollectionList.FirstOrDefault(p => p.Id == id);
-
-            if (completedSetCollection is null)
-                return;
-
-            await ExecuteAsync(async () =>
-            {
-                if (!await _context.UpdateItemAsync<CompletedSetCollection>(completedSetCollection))
-                {
-                    await Shell.Current.DisplayAlert("Error", "Error occured when updating Completed Set Collection.", "OK");
-                }
-
-                await LoadCompletedSetCollectionsAsync();
-            });
-        }
-
-        [RelayCommand]
-        private async Task DeleteCompletedSetCollectionAsync(int id)
-        {
-            CompletedSetCollection completedSetCollection = CompletedSetCollectionList.FirstOrDefault(p => p.Id == id);
-
-            if (completedSetCollection is null)
-                return;
-
-            await ExecuteAsync(async () =>
-            {
-                if (!await _context.DeleteItemAsync<CompletedSetCollection>(completedSetCollection))
-                {
-                    await Shell.Current.DisplayAlert("Error", "Error occured when updating Completed Set Collection.", "OK");
-                }
-            });
-
-            await LoadCompletedSetCollectionsAsync();
-
-            await DeleteCompletedSetsByCompletedSetCollectionId(id);
         }
 
         [RelayCommand]
@@ -186,56 +112,29 @@ namespace LocalLiftLog.ViewModels
             await LoadCompletedSetsAsync();
         }
 
-        private async Task DeleteCompletedSetsByCompletedSetCollectionId(int id)
-        {
-            Expression<Func<CompletedSet, bool>> predicate = entity => entity.CompletedSetCollectionId == id;
-
-            IEnumerable<CompletedSet> filteredList = null;
-            try
-            {
-                filteredList = await _context.GetFilteredAsync<CompletedSet>(predicate);
-            }
-            catch
-            {
-                await Shell.Current.DisplayAlert("Error", "An error occured when trying to load Set Templates.", "OK");
-            }
-
-            foreach (var item in filteredList)
-            {
-                await ExecuteAsync(async () =>
-                {
-                    if (!await _context.DeleteItemAsync<CompletedSet>(item))
-                    {
-                        await Shell.Current.DisplayAlert("Error", "Error occured when deleting Set Template.", "OK");
-                    }
-                });
-            }
-
-            await LoadCompletedSetsAsync();
-        }
-
         [RelayCommand]
         private async Task SetCompleteCompletedSetCollection(int id)
         {
-            CompletedSetCollection completedSetCollection = CompletedSetCollectionList.FirstOrDefault(p => p.Id == id);
+            //CompletedSetCollection completedSetCollection = CompletedSetCollectionList.FirstOrDefault(p => p.Id == id);
 
-            if (completedSetCollection is null)
-                return;
+            //if (completedSetCollection is null)
+            //    return;
 
-            string currentDateTimeString = DateTimeHelper.GetCurrentFormattedDateTime();
+            //string currentDateTimeString = DateTimeHelper.GetCurrentFormattedDateTime();
 
-            completedSetCollection.IsCompleted = true;
-            completedSetCollection.DateCompleted = currentDateTimeString;
+         
+            //completedSetCollection.IsCompleted = true;
+            //completedSetCollection.DateCompleted = currentDateTimeString;
 
-            await ExecuteAsync(async () =>
-            {
-                if (!await _context.UpdateItemAsync<CompletedSetCollection>(completedSetCollection))
-                {
-                    await Shell.Current.DisplayAlert("Error", "Error occured when updating Completed Set Collection.", "OK");
-                }
+            //await ExecuteAsync(async () =>
+            //{
+            //    if (!await _context.UpdateItemAsync<CompletedSetCollection>(completedSetCollection))
+            //    {
+            //        await Shell.Current.DisplayAlert("Error", "Error occured when updating Completed Set Collection.", "OK");
+            //    }
 
-                await LoadCompletedSetCollectionsAsync();
-            });
+            //    await LoadCompletedSetCollectionsAsync();
+            //});
         }
 
         [RelayCommand]
