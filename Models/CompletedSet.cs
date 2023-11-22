258117bddb90c335d8a57f6d1913d46a6fcc253f
Viktor <vikt.mag@gmail.com>
2023-11-22 02:06:54 +0100

    Added lift related properties to CompletedSet model

--- Models/CompletedSet.cs
+++ Models/CompletedSet.cs
@@ -15,5 +15,12 @@ namespace LocalLiftLog.Models
         public string DateCompleted { get; set; }
         public int CompletedWorkoutId { get; set; }
         public int Weight { get; set; }
+        public int Reps { get; set; }
+        public int Rir { get; set; }
+        public int Rpe { get; set; }
+        public int Time { get; set; }
+        public int Distance { get; set; }
+        public int CardioResistance { get; set; }
+        public bool IsUsingBodyWeightAsWeight { get; set; }
     }
 }
