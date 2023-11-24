52b6cf6cf6722123ec4078bfc290d8bba1502d3d
Viktor <vikt.mag@gmail.com>
2023-11-24 04:12:52 +0100

    Created TimePeriod model

---
+++ Models/TimePeriod.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class TimePeriod
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
+        public bool IsPeriodOngoing { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; } 
+    }
+}
