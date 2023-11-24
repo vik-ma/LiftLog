734f5279bcc8850d62236e7f55f75e87109a2f61
Viktor <vikt.mag@gmail.com>
2023-11-24 04:27:10 +0100

    Created TimePeriodViewModel

---
+++ ViewModels/TimePeriodViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Pages;
+using LocalLiftLog.Helpers;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq.Expressions;
+
+namespace LocalLiftLog.ViewModels
+{
+    public partial class TimePeriodViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public TimePeriodViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<TimePeriod> timePeriodList = new();
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
