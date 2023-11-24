c32ffd9ce04a7c6d62a981529a433fa7b3d267f7
Viktor <vikt.mag@gmail.com>
2023-11-24 04:31:51 +0100

    Created TimePeriodListPage

---
+++ Pages/TimePeriodListPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace LocalLiftLog.Pages;
+using LocalLiftLog.ViewModels;
+
+public partial class TimePeriodListPage : ContentPage
+{
+	private readonly TimePeriodViewModel _viewModel;
+	public TimePeriodListPage(TimePeriodViewModel viewModel)
+	{
+		InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.LoadTimePeriodsAsync();
+    }
+}
