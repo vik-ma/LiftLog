0b4a0c0640d6bbd87c8f22bf498ebd005412a80c
Viktor <vikt.mag@gmail.com>
2023-11-25 20:10:41 +0100

    Created Exercise model

---
+++ Models/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public HashSet<int> ExerciseGroupSet { get; set; }
+    }
+}
