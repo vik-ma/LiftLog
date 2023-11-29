02bea19dd84cac5942727315277c4a0cfccc0ab2
Viktor <vikt.mag@gmail.com>
2023-11-29 23:16:03 +0100

    Created ExerciseListViewModel

---
+++ ViewModels/ExerciseListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Helpers;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq.Expressions;
+
+namespace LocalLiftLog.ViewModels
+{
+    public partial class ExerciseListViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+        private readonly ExerciseDataManager _exerciseData;
+
+        public ExerciseListViewModel(DatabaseContext context, ExerciseDataManager exerciseData)
+        {
+            _context = context;
+            _exerciseData = exerciseData;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Exercise> exerciseList = new();
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
