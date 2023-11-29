cb5c8a839e55e46445ebe52dbff602f9d092c7b9
Viktor <vikt.mag@gmail.com>
2023-11-30 00:02:50 +0100

    Added LoadExercisesAsync function to ExerciseListViewModel

--- ViewModels/ExerciseListViewModel.cs
+++ ViewModels/ExerciseListViewModel.cs
@@ -27,6 +27,44 @@ namespace LocalLiftLog.ViewModels
         [ObservableProperty]
         private ObservableCollection<Exercise> exerciseList = new();
 
+        public async Task LoadExercisesAsync()
+        {
+            await ExecuteAsync(async () =>
+            {
+                ExerciseList.Clear();
+
+                var exercises = await _exerciseData.GetFullExerciseList();
+
+                if (exercises is not null && exercises.Any())
+                {
+                    exercises ??= new ObservableCollection<Exercise>();
+
+                    foreach (var exercise in exercises)
+                    {
+                        ExerciseList.Add(exercise);
+                    }
+                }
+            });
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
         [RelayCommand]
         static async Task GoBack()
         {
