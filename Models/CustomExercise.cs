67fd6c40359b3710070adcee96c06cc7d55d896d
Viktor <vikt.mag@gmail.com>
2023-11-30 00:53:39 +0100

    Added ValidateExerciseGroups function to CustomExercise class

--- Models/CustomExercise.cs
+++ Models/CustomExercise.cs
@@ -28,5 +28,29 @@ namespace LocalLiftLog.Models
         public bool IsExerciseGroup13 { get; set; }
         public bool IsExerciseGroup14 { get; set; }
         public bool IsExerciseGroup15 { get; set; }
+
+        public bool ValidateExerciseGroups()
+        {
+            // Check if at least one Exercise Group has been added
+            // Returns true if at least one IsExerciseGroup bool is set to true, otherwise false
+
+            Type type = this.GetType();
+
+            // Get properties that start with "IsExerciseGroup"
+            var groupProperties = type.GetProperties()
+                                      .Where(prop => prop.PropertyType == typeof(bool) && prop.Name.StartsWith("IsExerciseGroup"))
+                                      .ToList();
+
+            foreach (var property in groupProperties)
+            {
+                if ((bool)property.GetValue(this))
+                {
+                    // If at least one IsExerciseGroup property is set to true
+                    return true;
+                }
+            }
+            // If no IsExerciseGroup properties are set to true
+            return false;
+        }
     }
 }
