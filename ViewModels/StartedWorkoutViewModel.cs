2a6bef6d3b93c8abc7641b9a280a69adc0321873
Viktor <vikt.mag@gmail.com>
2023-12-02 12:14:37 +0100

    Created StartedWorkoutViewModel class

---
+++ ViewModels/StartedWorkoutViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.ViewModels
+{
+    [QueryProperty(nameof(CompletedWorkout), nameof(CompletedWorkout))]
+    public partial class StartedWorkoutViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public StartedWorkoutViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+    }
+
+    
+}
