c046cbc41f367a1fd67a5e59b127cc9d5e286768
Viktor <vikt.mag@gmail.com>
2023-12-02 10:38:40 +0100

    Updated ExerciseList ExerciseGroupSets

--- Data/ExerciseDataManager.cs
+++ Data/ExerciseDataManager.cs
@@ -34,13 +34,14 @@ namespace LocalLiftLog.Data
             ExerciseList = new List<Exercise>
             {
                 new Exercise { Name="Bench Press", ExerciseGroupSet=new HashSet<int>(new[] { 0, 1 }) },
-                new Exercise { Name="Hammer Curl", ExerciseGroupSet=new HashSet<int>(new[] { 2, 11 }) },
-                new Exercise { Name="Lateral Raise", ExerciseGroupSet=new HashSet<int>(new[] { 3 }) },
-                new Exercise { Name="Deadlift", ExerciseGroupSet=new HashSet<int>(new[] { 4, 5, 6, 7, 8, 9, 12 }) },
-                new Exercise { Name="Calf Raise", ExerciseGroupSet=new HashSet<int>(new[] { 10 }) },
-                new Exercise { Name="Crush Gripper", ExerciseGroupSet=new HashSet<int>(new[] { 13 }) },
-                new Exercise { Name="Running", ExerciseGroupSet=new HashSet<int>(new[] { 14 }) },
-                new Exercise { Name="Other", ExerciseGroupSet=new HashSet<int>(new[] { 15 }) },
+                new Exercise { Name="Hammer Curl", ExerciseGroupSet=new HashSet<int>(new[] { 2, 3 }) },
+                new Exercise { Name="Lateral Raise", ExerciseGroupSet=new HashSet<int>(new[] { 4 }) },
+                new Exercise { Name="Deadlift", ExerciseGroupSet=new HashSet<int>(new[] { 5, 6, 7, 8, 9, 10, 11 }) },
+                new Exercise { Name="Calf Raise", ExerciseGroupSet=new HashSet<int>(new[] { 12 }) },
+                new Exercise { Name="Sit-Ups", ExerciseGroupSet=new HashSet<int>(new[] { 12 }) },
+                new Exercise { Name="Crush Gripper", ExerciseGroupSet=new HashSet<int>(new[] { 14 }) },
+                new Exercise { Name="Running", ExerciseGroupSet=new HashSet<int>(new[] { 15 }) },
+                new Exercise { Name="Other", ExerciseGroupSet=new HashSet<int>(new[] { 16 }) },
             };
 
             await UpdateFullExerciseList();
