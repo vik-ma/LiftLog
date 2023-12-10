f35ada8d675cbce6d3e0c4e301aec7fe20ca35e5
Viktor <vikt.mag@gmail.com>
2023-12-11 00:29:05 +0100

    Created SetWorkoutTemplatePackage class

---
+++ Models/SetWorkoutTemplatePackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class SetWorkoutTemplatePackage
+    {
+        public WorkoutTemplate workoutTemplate { get; set; }
+        public SetTemplate setTemplate { get; set; }
+    }
+}
