bd7ebdcc8f842bd520d82ab474d7cabfb8c25bbc
Viktor <vikt.mag@gmail.com>
2023-12-14 13:37:49 +0100

    Added SetListOrder property to WorkoutTemplate model

--- Models/WorkoutTemplate.cs
+++ Models/WorkoutTemplate.cs
@@ -13,6 +13,7 @@ namespace LocalLiftLog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int SetTemplateCollectionId { get; set; }
+        public string SetListOrder { get; set; }
     }
 
 }
