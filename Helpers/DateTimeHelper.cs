62989cab91cb2e946d7e31c51408e3db4de36765
Viktor <vikt.mag@gmail.com>
2023-12-18 11:30:59 +0100

    Added GetCurrentFormattedYmdDate and ValidateStartAndEndYmdDate functions to DateTimeHelper class

--- Helpers/DateTimeHelper.cs
+++ Helpers/DateTimeHelper.cs
@@ -13,12 +13,35 @@ namespace LocalLiftLog.Helpers
             string dateTimeString = DateTime.UtcNow.ToString();
             return dateTimeString;
         }
+        public static string GetCurrentFormattedYmdDate()
+        {
+            string ymdDateString = DateTime.UtcNow.ToString("yyyyMMdd");
+            return ymdDateString;
+        }
 
         public static bool ValidateStartAndEndDate(string startDateTime, string endDateTime)
         {
             // Parse the DateTime strings into DateTime objects
             if (DateTime.TryParse(startDateTime, out DateTime start) &&
                 DateTime.TryParse(endDateTime, out DateTime end))
+            {
+                // Compare the DateTime objects
+                // Returns true if End DateTime occurs after Start DateTime
+                return start < end;
+            }
+
+            // Handle parsing errors if necessary
+            return false;
+        }
+
+        public static bool ValidateStartAndEndYmdDate(string startYmdDate, string endYmdDate)
+        {
+            // Define the expected date format
+            string dateFormat = "yyyyMMdd";
+
+            // Parse the DateTime strings into DateTime objects
+            if (DateTime.TryParseExact(startYmdDate, dateFormat, null, System.Globalization.DateTimeStyles.None, out DateTime start) &&
+                DateTime.TryParseExact(endYmdDate, dateFormat, null, System.Globalization.DateTimeStyles.None, out DateTime end))
             {
                 // Compare the DateTime objects
                 // Returns true if End DateTime occurs after Start DateTime
