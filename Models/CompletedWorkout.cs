672b7643ded11417e835b44698b5952e4a06988e
Viktor <vikt.mag@gmail.com>
2023-12-18 11:17:19 +0100

    Added WorkoutTemplateCollectionId property and changed name of DateCompleted to Date in CompletedWorkout model

--- Models/CompletedWorkout.cs
+++ Models/CompletedWorkout.cs
@@ -12,7 +12,8 @@ namespace LocalLiftLog.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int WorkoutTemplateId { get; set; }
+        public int WorkoutTemplateCollectionId { get; set; }
         public bool IsCompleted { get; set; }
-        public string DateCompleted { get; set; }
+        public string Date { get; set; }
     }
 }
