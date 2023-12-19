261c7876dabbd350d64e45e4d50305b8db3f4734
Viktor <vikt.mag@gmail.com>
2023-12-19 21:53:35 +0100

    Created SelectedWorkoutViewModel class

---
+++ ViewModels/SelectWorkoutViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.ViewModels
+{
+    public partial class SelectWorkoutViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public SelectWorkoutViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
