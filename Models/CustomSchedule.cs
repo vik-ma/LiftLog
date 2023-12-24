094f89fc551686d02af1c94bbd1588c93a4a4bfa
Viktor <vikt.mag@gmail.com>
2023-12-24 14:18:08 +0100

    Removed DayXWorkoutTemplateCollection properties from WeeklySchedule and CustomSchedule models

--- Models/CustomSchedule.cs
+++ Models/CustomSchedule.cs
@@ -15,20 +15,5 @@ namespace LocalLiftLog.Models
         public int WorkoutRoutineId { get; set; }
         public int NumDaysInSchedule { get; set; }
         public string ScheduleStartDate { get; set; }
-        public int Day1WorkoutTemplateCollectionId { get; set; }
-        public int Day2WorkoutTemplateCollectionId { get; set; }
-        public int Day3WorkoutTemplateCollectionId { get; set; }
-        public int Day4WorkoutTemplateCollectionId { get; set; }
-        public int Day5WorkoutTemplateCollectionId { get; set; }
-        public int Day6WorkoutTemplateCollectionId { get; set; }
-        public int Day7WorkoutTemplateCollectionId { get; set; }
-        public int Day8WorkoutTemplateCollectionId { get; set; }
-        public int Day9WorkoutTemplateCollectionId { get; set; }
-        public int Day10WorkoutTemplateCollectionId { get; set; }
-        public int Day11WorkoutTemplateCollectionId { get; set; }
-        public int Day12WorkoutTemplateCollectionId { get; set; }
-        public int Day13WorkoutTemplateCollectionId { get; set; }
-        public int Day14WorkoutTemplateCollectionId { get; set; }
-
     }
 }
--- Models/WeeklySchedule.cs
+++ Models/WeeklySchedule.cs
@@ -14,13 +14,5 @@ namespace LocalLiftLog.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int WorkoutRoutineId { get; set; }
-        public int Day1WorkoutTemplateCollectionId { get; set; }
-        public int Day2WorkoutTemplateCollectionId { get; set; }
-        public int Day3WorkoutTemplateCollectionId { get; set; }
-        public int Day4WorkoutTemplateCollectionId { get; set; }
-        public int Day5WorkoutTemplateCollectionId { get; set; }
-        public int Day6WorkoutTemplateCollectionId { get; set; }
-        public int Day7WorkoutTemplateCollectionId { get; set; }
-
     }
 }
