f564cdcbb58f30ce840cfef74e4be308d52404f2
Viktor <vikt.mag@gmail.com>
2023-12-24 13:14:20 +0100

    Created WeekdayDictionary class

---
+++ Helpers/WeekdayDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Helpers
+{
+    public static class WeekdayDictionary
+    {
+        public static Dictionary<int, string> WeekdayDict { get; } = new()
+        {
+            { 0, "Monday" },
+            { 1, "Tuesday" },
+            { 2, "Wednesday" },
+            { 3, "Thursday" },
+            { 4, "Friday" },
+            { 5, "Saturday" },
+            { 6, "Sunday" }
+        };
+    };
+}
