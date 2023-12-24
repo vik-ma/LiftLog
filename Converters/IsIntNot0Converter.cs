f56725f467ea337deca071cac2cfa95755b70ea5
Viktor <vikt.mag@gmail.com>
2023-12-24 12:56:48 +0100

    Created IsIntNot0Converter class

---
+++ Converters/IsIntNot0Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Converters
+{
+    public class IsIntNot0Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+            {
+                return intValue != 0;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
