801142bbee7e4fa8a1309381ff76f766a4c11af5
Viktor <vikt.mag@gmail.com>
2023-12-27 14:23:46 +0100

    Added GetTrackingDict function to SetTemplate model

--- Models/SetTemplate.cs
+++ Models/SetTemplate.cs
@@ -1,4 +1,5 @@
-﻿using SQLite;
+﻿using Microsoft.Maui;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +26,29 @@ namespace LocalLiftLog.Models
         public bool IsUsingBodyWeightAsWeight { get; set; }
 
         public SetTemplate Clone() => MemberwiseClone() as SetTemplate;
+
+        public Dictionary<bool, List<string>> GetTrackingDict()
+        {
+            List<string> isTrackingList = new();
+            List<string> isNotTrackingList = new();
+
+            (this.IsWarmupSet ? isTrackingList : isNotTrackingList).Add("Warmup");
+            (this.IsTrackingWeight ? isTrackingList : isNotTrackingList).Add("Weight");
+            (this.IsTrackingReps ? isTrackingList : isNotTrackingList).Add("Reps");
+            (this.IsTrackingRir ? isTrackingList : isNotTrackingList).Add("RIR");
+            (this.IsTrackingRpe ? isTrackingList : isNotTrackingList).Add("RPE");
+            (this.IsTrackingTime ? isTrackingList : isNotTrackingList).Add("Time");
+            (this.IsTrackingDistance ? isTrackingList : isNotTrackingList).Add("Distance");
+            (this.IsTrackingCardioResistance ? isTrackingList : isNotTrackingList).Add("Cardio Resistance");
+            (this.IsUsingBodyWeightAsWeight ? isTrackingList : isNotTrackingList).Add("Count Body Weight");
+
+            Dictionary<bool, List<string>> setTrackingDict = new()
+            {
+                { true, isTrackingList },
+                { false, isNotTrackingList }
+            };
+
+            return setTrackingDict;
+        }
     }
 }
