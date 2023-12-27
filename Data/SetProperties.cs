3abad5e848a3eac6fddbde1e45bbb4ec255763cc
Viktor <vikt.mag@gmail.com>
2023-12-27 14:34:28 +0100

    Created SetProperties class

---
+++ Data/SetProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Data
+{
+    public static class SetProperties
+    {
+        public static readonly List<string> SetPropertyList = new()
+        {
+            "Warmup",
+            "Weight",
+            "Reps",
+            "RIR",
+            "RPE",
+            "Time",
+            "Distance",
+            "Cardio Resistance",
+            "Count Body Weight"
+        };
+    }
+}
