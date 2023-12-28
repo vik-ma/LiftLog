8fde9f0fa1cf0bff295a5006003ac823339ae00c
Viktor <vikt.mag@gmail.com>
2023-12-28 15:19:03 +0100

    Created UserPreferences model

---
+++ Models/UserPreferences.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class UserPreferences
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public bool IsUsingMetricUnits { get; set; }
+        public int ActiveWorkoutRoutineId { get; set; }
+        public int ActiveTimePeriodId { get; set; }
+        public bool ShowCompletedSetTimestamp { get; set; }
+        public int DefaultBarbellWeight { get; set; }
+        public int DefaultDumbellWeight { get; set; }
+    }
+}
