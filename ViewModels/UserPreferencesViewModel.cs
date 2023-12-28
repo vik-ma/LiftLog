87371e27e32f63c4d89bd95bb07694ded03fa117
Viktor <vikt.mag@gmail.com>
2023-12-28 15:36:27 +0100

    Created UserPreferencesViewModel class

---
+++ ViewModels/UserPreferencesViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LocalLiftLog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.ViewModels
+{
+    public partial class UserPreferencesViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public UserPreferencesViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
