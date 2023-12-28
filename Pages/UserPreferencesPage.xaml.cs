872217f0c1b212343393f61eabef8013811b6a65
Viktor <vikt.mag@gmail.com>
2023-12-28 15:40:21 +0100

    Created UserPreferencesPage

---
+++ Pages/UserPreferencesPage.xaml.cs
@@ -0,0 +1,18 @@
+namespace LocalLiftLog.Pages;
+using LocalLiftLog.ViewModels;
+
+public partial class UserPreferencesPage : ContentPage
+{
+	private readonly UserPreferencesViewModel _viewModel;
+	public UserPreferencesPage(UserPreferencesViewModel viewModel)
+	{
+		InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+}
