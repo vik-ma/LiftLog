e491a358b6530133674b16de1488ed2241ca093d
Viktor <vikt.mag@gmail.com>
2023-12-31 15:10:40 +0100

    Renamed AreStringsEqualMultiBindingConverter to AreStringsOrIntsEqualMultiBindingConverter

--- Converters/AreStringsOrIntsEqualMultiBindingConverter.cs
+++ Converters/AreStringsOrIntsEqualMultiBindingConverter.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 
 namespace LocalLiftLog.Converters
 {
-    public class AreStringsEqualMultiBindingConverter : IMultiValueConverter
+    public class AreStringsOrIntsEqualMultiBindingConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values[0] is string stringValue0 && values[1] is string stringValue1)
             {
                 return stringValue0 == stringValue1;
+            }
+
+            if (values[0] is int intValue0 && values[1] is int intValue1)
+            {
+                return intValue0 == intValue1;
             }
 
             return false;
