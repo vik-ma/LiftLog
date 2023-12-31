8a8033b4062430c74366f651e8c79595ccc1401c
Viktor <vikt.mag@gmail.com>
2023-12-31 14:28:02 +0100

    Created AreStringsEqualMultiBindingConverter class

---
+++ Converters/AreStringsEqualMultiBindingConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Converters
+{
+    public class AreStringsEqualMultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is string stringValue0 && values[1] is string stringValue1)
+            {
+                return stringValue0 == stringValue1;
+            }
+
+            return false;
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
