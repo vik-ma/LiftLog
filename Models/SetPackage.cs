0aa2a3675534000865f25662824a96d45e957cdd
Viktor <vikt.mag@gmail.com>
2024-01-01 08:23:26 +0100

    Created SetPackage model

---
+++ Models/SetPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class SetPackage
+    {
+        public SetTemplate SetTemplate { get; set; }
+        public CompletedSet CompletedSet { get; set; }
+    }
+}
