b9386b8624d5d80cc671213f4f9e893b05785337
Viktor <vikt.mag@gmail.com>
2024-01-02 16:22:52 +0100

    Created DateTimeToTimestampConverter class

---
+++ Converters/DateTimeToTimestampConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocalLiftLog.Helpers;
+
+namespace LocalLiftLog.Converters
+{
+    public class DateTimeToTimestampConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if (value is string dateString)
+            //{
+            //    if (parameter is string parameterString && parameterString == "IncludeDate")
+            //    {
+            //        return DateTimeHelper.fpr
+            //    }
+
+            //    return DateTimeHelper.FormatDateTimeStringToTimestamp(dateString);
+            //}
+
+            return "Invalid DateTime";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
