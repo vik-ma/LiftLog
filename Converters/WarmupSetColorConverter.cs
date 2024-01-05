0e190e317ce6442720d432f46e8b97c50cbdbc89
Viktor <vikt.mag@gmail.com>
2024-01-05 20:59:37 +0100

    Created WarmupSetColorConverter class

---
+++ Converters/WarmupSetColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Converters
+{
+    public class WarmupSetColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+            
+            Color veryLightOrangeColor = ColorResource["VeryLightOrange"] as Color;
+            Color mediumBrownColor = ColorResource["MediumBrown"] as Color;
+            Color veryLightGrayColor = ColorResource["VeryLightGray"] as Color;
+            Color veryDarkGrayColor = ColorResource["VeryDarkGray"] as Color;
+
+            if (value is bool boolValue && boolValue)
+            {
+                // If Set is Warmup-Set
+                if (parameter is string contextTrue && contextTrue == "Background")
+                {
+                    // Background Color
+                    return veryLightOrangeColor;
+                }
+                // Text Color
+                return mediumBrownColor;
+            }
+
+            // If Set is NOT Warmup-Set
+            if (parameter is string contextFalse && contextFalse == "Background")
+            {
+                // Background Color
+                return veryLightGrayColor;
+            }
+            // Text Color
+            return veryDarkGrayColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
