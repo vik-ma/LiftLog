9dcf1b0221fc97144d11920f2be16294ac9c4bcf
Viktor <vikt.mag@gmail.com>
2024-01-09 08:38:01 +0100

    Created ConstantsHelper class

---
+++ Helpers/ConstantsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Helpers
+{
+    public static class ConstantsHelper
+    {
+        public static readonly int SetTemplateDefaultMinValue = 0;
+        public static readonly int SetTemplateDefaultMaxValue = 999;
+
+        public static readonly int CompletedSetMinValue = 0;
+        public static readonly int CompletedSetMaxValue = 999;
+    }
+}
