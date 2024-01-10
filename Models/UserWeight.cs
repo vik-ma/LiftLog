6a4af4796688c2f11315cd04532df63df8cb1742
Viktor <vikt.mag@gmail.com>
2024-01-11 00:22:58 +0100

    Created UserWeight model

---
+++ Models/UserWeight.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLiftLog.Models
+{
+    public class UserWeight
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int BodyWeight { get; set; }
+        public string DateTime { get; set; }
+    }
+}
