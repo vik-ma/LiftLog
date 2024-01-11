53832323d1f76ebde3c1e371c1efd8ccc357f312
Viktor <vikt.mag@gmail.com>
2024-01-11 01:49:30 +0100

    Created UserWeightViewModel

---
+++ ViewModels/UserWeightViewModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LocalLiftLog.Data;
+using LocalLiftLog.Models;
+using LocalLiftLog.Pages;
+using LocalLiftLog.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LocalLiftLog.ViewModels
+{
+    public partial class UserWeightViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public UserWeightViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        public ObservableCollection<UserWeight> userWeightList = new();
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        public async Task LoadUserWeightListAsync()
+        {
+            await ExecuteAsync(async () =>
+            {
+                UserWeightList.Clear();
+
+                var weightList = await _context.GetAllAsync<UserWeight>();
+
+                if (weightList is not null && weightList.Any())
+                {
+                    weightList ??= new ObservableCollection<UserWeight>();
+
+                    foreach (var set in weightList)
+                    {
+                        UserWeightList.Add(set);
+                    }
+                }
+            });
+        }
+    }
+}
