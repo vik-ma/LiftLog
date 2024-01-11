6d3c1ca1c59adc72c583e671bbc1ad8566e9e43a
Viktor <vikt.mag@gmail.com>
2024-01-11 01:56:58 +0100

    Created UserWeightPage

---
+++ Pages/UserWeightPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace LocalLiftLog.Pages;
+using LocalLiftLog.ViewModels;
+
+public partial class UserWeightPage : ContentPage
+{
+    private readonly UserWeightViewModel _viewModel;
+    public UserWeightPage(UserWeightViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.LoadUserWeightListAsync();
+    }
+}
