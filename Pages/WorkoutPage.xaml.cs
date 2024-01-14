4c16dd42896956f6865b0d27d6c61a1c076bf595
Viktor <vikt.mag@gmail.com>
2024-01-14 09:31:55 +0100

    Created WorkoutPage and WorkoutViewModel

---
+++ Pages/WorkoutPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace LocalLiftLog.Pages;
+
+public partial class WorkoutPage : ContentPage
+{
+    private readonly WorkoutViewModel _viewModel;
+    public WorkoutPage(WorkoutViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+}
---
+++ ViewModels/WorkoutViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace LocalLiftLog.ViewModels
+{
+    [QueryProperty(nameof(Workout), nameof(Workout))]
+    public partial class WorkoutViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        [ObservableProperty]
+        private UserPreferencesViewModel userSettingsViewModel;
+
+        [ObservableProperty]
+        private Workout workout;
+
+        public WorkoutViewModel(DatabaseContext context, UserPreferencesViewModel userSettings)
+        {
+            _context = context;
+            userSettingsViewModel = userSettings;
+        }
+
+        [RelayCommand]
+        static async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        #nullable enable
+        private async Task ExecuteAsync(Func<Task> operation)
+        {
+            try
+            {
+                #nullable disable
+                await operation?.Invoke();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
