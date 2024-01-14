526b9aeea518ff37359fac42aa98736647de7025
Viktor <vikt.mag@gmail.com>
2024-01-14 09:24:40 +0100

    Created Workout model

---
+++ Models/Workout.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalLiftLog.Models
+{
+    public class Workout
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int WorkoutTemplateId { get; set; }
+        public string Date { get; set; }
+    }
+}
