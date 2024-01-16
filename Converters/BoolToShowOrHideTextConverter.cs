0ac7d1db5f125ac4d4928243c6d569489a77d382
Viktor <vikt.mag@gmail.com>
2024-01-16 15:26:27 +0100

    Added BoolToShowOrHideTextConverter

---
+++ Converters/BoolToShowOrHideTextConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class BoolToShowOrHideTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                if (parameter is string context)
+                {
+                    if (boolValue) return $"Hide {context}";
+                    else return $"Show {context}";
+                }
+                return "Invalid Parameter";
+            }
+            return "Invalid Input";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
