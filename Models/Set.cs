0348a8e01cf23df618b313ac229d878fb491a192
Viktor <vikt.mag@gmail.com>
2024-01-19 12:16:50 +0100

    Created Set model

---
+++ Models/Set.cs
@@ -0,0 +1,22 @@
+﻿namespace LocalLiftLog.Models
+{
+    public partial class Set : ObservableObject
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ExerciseName { get; set; }
+        [ObservableProperty]
+        public string note;
+        public bool IsWarmupSet { get; set; }
+        [ObservableProperty]
+        public string timeCompleted;
+        public double Weight { get; set; }
+        public int Reps { get; set; }
+        public int Rir { get; set; }
+        public int Rpe { get; set; }
+        public int TimeInSeconds { get; set; }
+        public double Distance { get; set; }
+        public double CardioResistance { get; set; }
+        public int PercentCompleted { get; set; }
+    }
+}
