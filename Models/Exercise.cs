ffccdd3271ed4e95483229d69768a6c859e9de15
Viktor <vikt.mag@gmail.com>
2024-01-20 13:42:55 +0100

    Added ExerciseGroupSetString property to Exercise model along with functions to convert HashSet<int> to string and back

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -4,5 +4,34 @@
     {
         public string Name { get; set; }
         public HashSet<int> ExerciseGroupSet { get; set; }
+        public string ExerciseGroupSetString { get; set; }
+
+        public HashSet<int> GetExerciseGroupHashSet()
+        {
+            HashSet<int> exerciseGroupHashSet = new();
+
+            string[] groupStrings = ExerciseGroupSetString.Split(',');
+
+            foreach (var group in groupStrings)
+            {
+                if (int.TryParse(group.Trim(), out int groupInt))
+                {
+                    exerciseGroupHashSet.Add(groupInt);
+                }
+            }
+
+            return exerciseGroupHashSet;
+        }
+
+        public void SetExerciseGroupSetString(HashSet<int> exerciseGroupHashSet)
+        {
+            // Do not update empty HashSets
+            if (exerciseGroupHashSet is null || !exerciseGroupHashSet.Any()) return;
+
+            // Create string of HashSet with comma separated values
+            string hashSetString = string.Join(",", exerciseGroupHashSet);
+
+            ExerciseGroupSetString = hashSetString;
+        }
     }
 }
