839c7368bb0540b0cbab8ff63d187e48b7c281b0
Viktor <vikt.mag@gmail.com>
2024-01-24 11:54:17 +0100

    Created SetTemplateExercisePackage model

---
+++ Models/SetTemplateExercisePackage.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalLiftLog.Models
+{
+    public class SetTemplateExercisePackage
+    {
+        public SetTemplate SetTemplate { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+}
