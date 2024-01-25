71235dfc3b0674f6ddcebe6f29b0a9adf867649d
Viktor <vikt.mag@gmail.com>
2024-01-25 20:48:51 +0100

    Created InvalidExerciseColorConverter and added converter to WorkoutDetailsPage

---
+++ Converters/InvalidExerciseColorConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class InvalidExerciseColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+
+            Color blackColor = ColorResource["Black"] as Color;
+            Color redColor = ColorResource["MediumRed"] as Color;
+
+            if (value is string stringValue && stringValue == "Invalid Exercise")
+            {
+                return redColor;
+            }
+            return blackColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
