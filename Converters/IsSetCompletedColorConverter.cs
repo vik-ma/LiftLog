a796f513144d9a63a961c3c1d12a6669078725d9
Viktor <vikt.mag@gmail.com>
2024-01-26 22:40:58 +0100

    Create IsSetCompletedColorConverter and added converter to SetList CollectionView in WorkoutListPage

---
+++ Converters/IsSetCompletedColorConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class IsSetCompletedColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+
+            Color mediumGreenColor = ColorResource["MediumGreen"] as Color;
+            Color mediumRedColor = ColorResource["MediumRed"] as Color;
+
+            if (value is bool boolValue && boolValue)
+            {
+                return mediumGreenColor;   
+            }
+            return mediumRedColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
