a0d8833a1034e764e837a9b97b2d09b87d0f3cda
Viktor <vikt.mag@gmail.com>
2024-02-13 16:30:31 +0100

    Add Note property to Exercise model

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -6,6 +6,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string ExerciseGroupSetString { get; set; }
+        public string Note { get; set; }
 
         public HashSet<int> GetExerciseGroupHashSet()
         {
