9fdcdd19867225bcc4b9f00172da786f3fb91049
Viktor <vikt.mag@gmail.com>
2024-02-13 12:05:39 +0100

    Add FlippedExerciseGroupDict to ExerciseGroupDictionary

--- Data/ExerciseGroupDictionary.cs
+++ Data/ExerciseGroupDictionary.cs
@@ -25,6 +25,29 @@
             { 18, "Legs" },
         };
 
+        public static Dictionary<string, int> FlippedExerciseGroupDict { get; } = new()
+        {
+            { "Chest", 0 },
+            { "Triceps", 1 },
+            { "Biceps", 2 },
+            { "Forearms", 3 },
+            { "Shoulders", 4 },
+            { "Upper Back", 5 },
+            { "Mid Back (Lats)", 6 },
+            { "Lower Back", 7 },
+            { "Glutes", 8 },
+            { "Adductors", 9 },
+            { "Quadriceps", 10 },
+            { "Hamstrings", 11 },
+            { "Calves", 12 },
+            { "Core", 13 },
+            { "Grip", 14 },
+            { "Cardio", 15 },
+            { "Other", 16 },
+            { "Back", 17 },
+            { "Legs", 18 },
+        };
+
         // Change the value of this property when adding/removing ExerciseGroupDict key(s) to the new max value
         public static int ExerciseGroupMaxValue { get; } = 18;
 
