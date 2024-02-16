f66ac6fcc3dc32518c5c725c6dfa7ecd914c276a
Viktor <vikt.mag@gmail.com>
2024-02-16 08:37:49 +0100

    Create SetGroup class

---
+++ Models/SetGroup.cs
@@ -0,0 +1,13 @@
+﻿namespace LocalLiftLog.Models
+{
+    public class SetGroup : List<Set>
+    {
+        public string Date { get; private set; }
+
+        public SetGroup(string date, List<Set> sets) : 
+            base(sets) 
+        {
+            Date = date;
+        }
+    }
+}
