85932d78816721281fb091c41243598fdb158ddd
Viktor <vikt.mag@gmail.com>
2024-02-17 17:13:41 +0100

    Change SetListDragColorConverter to MultiBinding

---
+++ Converters/SetListDragColorMultiBindingConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class SetListDragColorMultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+
+            Color isItemDraggedColor = ColorResource["MediumGray"] as Color;
+            Color isDraggedOverColor = ColorResource["VeryLightGray"] as Color;
+            Color defaultColor = ColorResource["White"] as Color;
+
+            if (values[1] is bool isItemDragged && isItemDragged)
+            {
+                return isItemDraggedColor;
+            }
+
+            if (values[0] is bool isDraggedOver && isDraggedOver)
+            {
+                return isDraggedOverColor;
+            }
+
+            return defaultColor;
+
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
