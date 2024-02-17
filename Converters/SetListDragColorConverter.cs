7c457060a7c0500a60eb451bd1ed4148a82d3482
Viktor <vikt.mag@gmail.com>
2024-02-17 11:25:44 +0100

    Create SetListDragColorConverter

---
+++ Converters/SetListDragColorConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class SetListDragColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+
+            Color mediumGreenColor = ColorResource["MediumGreen"] as Color;
+
+            Color transparentColor = Color.FromArgb("#00FFFFFF");
+
+            if (value is bool boolValue && boolValue)
+            {
+                return mediumGreenColor;
+            }
+
+            return transparentColor;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
