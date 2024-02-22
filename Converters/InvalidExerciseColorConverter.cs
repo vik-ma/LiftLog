9f05462237dbae4b419a5450427bba90b58ca0c1
Viktor <vikt.mag@gmail.com>
2024-02-22 11:14:18 +0100

    Add parameter to change Border color to InvalidExerciseColorConverter

--- Converters/InvalidExerciseColorConverter.cs
+++ Converters/InvalidExerciseColorConverter.cs
@@ -6,14 +6,20 @@
         {
             ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
 
-            Color normalColor = ColorResource["Black"] as Color;
+            Color normalTextColor = ColorResource["Black"] as Color;
+            Color normalBorderColor = ColorResource["Primary"] as Color;
             Color invalidColor = ColorResource["MediumRed"] as Color;
 
+
             if (value is bool isInvalid && isInvalid)
-            {
                 return invalidColor;
+
+            if (parameter is string parameterString && parameterString == "Border")
+            {
+                return normalBorderColor;
             }
-            return normalColor;
+
+            return normalTextColor;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
