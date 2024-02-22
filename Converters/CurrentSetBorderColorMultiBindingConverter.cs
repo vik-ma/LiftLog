a66f9cbf670cda4cd5f533a37740af3cee76432a
Viktor <vikt.mag@gmail.com>
2024-02-22 13:13:39 +0100

    Create CurrentSetBorderColorMultiBindingConverter class

---
+++ Converters/CurrentSetBorderColorMultiBindingConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace LocalLiftLog.Converters
+{
+    public class CurrentSetBorderColorMultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ResourceDictionary ColorResource = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
+
+            Color falseBorderColor = ColorResource["MediumGray"] as Color;
+            Color trueBorderColor = ColorResource["Primary"] as Color;
+
+            if (values[0] is SetExercisePackage set1 && values[1] is SetExercisePackage set2)
+            {
+                if (set1 == set2) return trueBorderColor;
+            }
+
+            return falseBorderColor;
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
