e66175c59672a30a6ae6b0e0cbcaa12d631fc424
Viktor <vikt.mag@gmail.com>
2024-02-22 13:21:09 +0100

    Add hover color to SetList Border in WorkoutOperatingSetPage

--- Pages/WorkoutOperatingSetPage.xaml.cs
+++ Pages/WorkoutOperatingSetPage.xaml.cs
@@ -10,6 +10,8 @@ public partial class WorkoutOperatingSetPage : ContentPage
     private readonly Color InactiveTabHoverBackgroundColor;
     private readonly Color InactiveTabDefaultTextColor;
     private readonly Color InactiveTabHoverTextColor;
+    private readonly Color SetDefaultBackgroundColor;
+    private readonly Color SetHoverBackgroundColor;
     public WorkoutOperatingSetPage(WorkoutViewModel viewModel)
 	{
 		InitializeComponent();
@@ -19,6 +21,8 @@ public partial class WorkoutOperatingSetPage : ContentPage
         InactiveTabHoverBackgroundColor = ColorResource["MediumLightGray"] as Color;
         InactiveTabDefaultTextColor = ColorResource["VeryDarkGray"] as Color;
         InactiveTabHoverTextColor = ColorResource["Black"] as Color;
+        SetDefaultBackgroundColor = ColorResource["White"] as Color;
+        SetHoverBackgroundColor = ColorResource["VeryLightGray"] as Color;
     }
 
     private void PointerGestureRecognizer_PointerEntered(object sender, PointerEventArgs e)
@@ -29,6 +33,11 @@ public partial class WorkoutOperatingSetPage : ContentPage
             frame.BackgroundColor = InactiveTabHoverBackgroundColor;
             label.TextColor = InactiveTabHoverTextColor;
         }
+
+        if (sender is Border border)
+        {
+            border.BackgroundColor = SetHoverBackgroundColor;
+        }
     }
 
     private void PointerGestureRecognizer_PointerExited(object sender, PointerEventArgs e)
@@ -38,6 +47,11 @@ public partial class WorkoutOperatingSetPage : ContentPage
             Label label = frame.Content as Label;
             frame.BackgroundColor = InactiveTabDefaultBackgroundColor;
             label.TextColor = InactiveTabDefaultTextColor;
+        }
+
+        if (sender is Border border)
+        {
+            border.BackgroundColor = SetDefaultBackgroundColor;
         }
     }
 }
